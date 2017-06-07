c027ad465925573a36debf605c2699ee72dd0ce7
Gokulakrishnan Kalaikovan <krishnangokul9@gmail.com>
2017-06-07 23:11:28 +0530

    Updated

--- anonymous/index.js
+++ anonymous/index.js
@@ -4,7 +4,7 @@ import Home from './components/home';
 import config from './config';
 import firebase from 'firebase/app';
 
-firebase.initializeApp(config.test);
+firebase.initializeApp(config.production);
 
 export default () => (
 	<div id="root">
